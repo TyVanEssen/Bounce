31883aa599efe7f93293aead3871dc4401b71bf1
TyVanEssen <tyvanessen@gmail.com>
2022-05-11 13:31:04 -0600

    Wrapup

--- src/components/bounce.ts
+++ src/components/bounce.ts
@@ -22,6 +22,12 @@ export default class Bounce extends Vue {
   private x: number = 300;
   private y: number = 560;
 
+  private dx: number = 10;
+  private dy: number = -20;
+
+  private gravity: [number, number] = [0, .98]
+  private reflection: number = 1.1
+
   private get circle(): Element {
     return this.$refs.circle as Element;
   }
@@ -33,6 +39,12 @@ export default class Bounce extends Vue {
 
   public bounce() { // this gets called every x ms -> update the position iteratively
     console.log(`called bounce function`);
+    
+    let bounced = this.bounceOnEdge(this.reflection)
+    this.subjectGravity()
+    this.reset_with_new()
+    this.updateXY()
+
     console.log(`after ${this.y}`);
   }
 
@@ -44,5 +56,45 @@ export default class Bounce extends Vue {
     return this.y.toString();
   }
 
+  public reset_with_new(){
+    if (Math.abs(this.dx) + Math.abs(this.dy) < 100) return
+    
+    this.x = 300
+    this.y = 300
+    this.dx = 0
+    this.dy = 0
 
+    this.gravity=[2 * Math.random() - 1, 2* Math.random() - 1]
+    this.reflection = 1 + (Math.random() /2)
+    console.info(`Reset with new params Gravity ${this.gravity} and Reflection ${this.reflection}`)
+  }
+
+  public subjectGravity(){
+    this.subjectForce(this.gravity)
+  }
+  public subjectForce(d : [number, number] = [0,0]){
+    this.dx += d[0]
+    this.dy += d[1]
+  }
+
+  public bounceOnEdge(preserve: number = 1): boolean {
+    let inBounds : [boolean, boolean] = this.inBoundingBox()
+    this.dx = inBounds[0] ? this.dx : -1 * preserve * this.dx
+    this.dy = inBounds[1] ? this.dy : -1 * preserve * this.dy
+    return inBounds.every((x => x))
+  }
+
+  private inBoundingBox(): [boolean, boolean]{
+    let out: [boolean, boolean] = [true, true]
+
+    if (this.x - this.radius < 0 || this.x + this.radius > this.height) out[0] = false
+    if (this.y - this.radius < 0 || this.y + this.radius > this.height) out[1] = false
+
+    return out
+  }
+
+  private updateXY() {
+    this.x = this.x + this.dx
+    this.y = this.y + this.dy
+  }
 }
